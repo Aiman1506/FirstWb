97611c1b7a826ea89e63ff45a05348ce6fb89fbe
Aiman1506 <ummeaiman1506.com>
2024-10-14 17:10:38 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> Hello Ainpman lalkot</h1>
+      <div>
+        <h2>
+          hello mai chutya hu aiman mera namm mera kamm kuch nahi bas sone ka 
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
